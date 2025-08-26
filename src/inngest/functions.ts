f4f357f208a3523dc5ad14f654ac3b46f1f17729
vikasya7 <vikasyaa19@gmail.com>
2025-08-27 01:55:14 +0530

    06-e2b-sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,3 +1,4 @@
+import {Sandbox} from "@e2b/code-interpreter"
 import {  openai, createAgent } from "@inngest/agent-kit"
 
 
@@ -5,11 +6,16 @@ import {  openai, createAgent } from "@inngest/agent-kit"
 
 import { inngest } from "./client";
 import { success } from "zod";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
+  async ({ event,step }) => {
+    const sandboxId= await step.run("get-sandbox-id", async()=>{
+      const sandbox=await Sandbox.create("fynx-nextjs-test2");
+      return sandbox.sandboxId;
+    })
     const codeAgent= createAgent({
       name:"summarizer",
       system: "You are an expert next.js developer. You write readable maintainable code. You write simple Next.js & React snippets.",
@@ -19,11 +25,17 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
     `write the following snippet: ${event.data.value}`,
     );
+
+    const sandboxUrl=await step.run("get-sandbox-url", async()=>{
+      const sandbox=await getSandbox(sandboxId);
+      const host= sandbox.getHost(3000);
+      return `https://${host}`;
+    })
    
     // download
     
     // summary
     
-    return { output};
+    return { output,sandboxUrl};
   },
 );
