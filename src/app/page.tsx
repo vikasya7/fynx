0e1585ba8ceda57698d7eaa7e1d21b13d9c62463
vikasya7 <vikasyaa19@gmail.com>
2025-08-29 00:18:21 +0530

    08-messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,27 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input';
 import { useTRPC } from '@/trpc/client'
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react'
 import { toast } from 'sonner';
 
 const page = () => {
   const [value,setValue]=useState("");
   const trpc=useTRPC();
-  const invoke=useMutation(trpc.invoke.mutationOptions({
+  const {data: messages}=useQuery(trpc.messages.getMany.queryOptions());
+  const createMessage=useMutation(trpc.messages.create.mutationOptions({
     onSuccess:()=>{
-      toast.success("background job started")
+      toast.success("Message created")
     }
   }))
   return (
     <div className='p-4 max-w-7xl mx-auto'>
       <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
-     <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({value:value})}>
+     <Button disabled={createMessage.isPending} 
+     onClick={()=>createMessage.mutate({value:value})}>
       Invoke background job
      </Button>
+     {JSON.stringify(messages,null,2)}
     </div>
   )
 }
