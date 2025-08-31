97b782df3c013b241c6e87d9c51f93d76411d7d9
vikasya7 <vikasyaa19@gmail.com>
2025-08-31 22:53:08 +0530

    09:projects

---
+++ src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,16 @@
+
+interface Props{
+    params: Promise<{
+       projectId: string;
+    }>
+};
+const Page =async ({params}: Props) => {
+    const {projectId}= await params;
+  return (
+    <div>
+        Project id:{projectId}
+    </div>
+  )
+}
+
+export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 
+import { projectsRouter } from '@/modules/projects/server/procedures';
 import {  createTRPCRouter } from '../init';
 
 import { messagesRouter } from '@/modules/messages/server/procedures';
 
 export const appRouter = createTRPCRouter({
   messages: messagesRouter,
+  projects: projectsRouter
   
 });
 // export type definition of API
